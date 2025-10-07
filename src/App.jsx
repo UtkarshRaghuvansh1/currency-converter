46476e6088a646c0c8c96a175e3ba9e1daf046d3
Utkarsh Ragghuvanshi <utkarshragghuvanshi@Utkarshs-MacBook-Air.local>
2025-10-07 10:37:03 +0530

    swap logic

--- src/App.jsx
+++ src/App.jsx
@@ -55,12 +55,13 @@ function App() {
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
               >
                 swap
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-              {/* <InputBox label="To" /> */}
+              <InputBox label={to} />
             </div>
             <button
               type="submit"
